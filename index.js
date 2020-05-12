d11c35b4a4bbe1522c5839008048959d5fd5fd0c
madhat5 <davidmalary@gmail.com>
2020-05-12 18:50:49 -0400

    selectors done, testing scrape

--- index.js
+++ index.js
@@ -16,7 +16,8 @@ const scrapeRes = [{
 const draftYrs = [2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019]
 
 async function draftLists(page) {
-    const data = []
+    const data = [];
+    
     for (var i = 0; i < draftYrs.length; i++) {
         await page.goto('https://www.pro-football-reference.com/years/' + draftYrs[i] + '/draft.htm')
 
@@ -26,8 +27,22 @@ async function draftLists(page) {
         // fs.writeFileSync('./test' + draftYrs[i] + '.html', html);
         
         $('#drafts > tbody > tr').each((i, el) => {
-            
+            const nameEl = $(el).find('td:nth-child(4) > a');
+            const posEl = $(el).find('td:nth-child(5)');
+            const pickNumEl = $(el).find('td:nth-child(2)');
+            const teamEl = $(el).find('td:nth-child(3)');
+            const ageEl = $(el).find('td:nth-child(6)');
+            const yearsPlayedEl = $(el).find('td:nth-child(7)');
+            const collegeEl = $(el).find('td:nth-child(28)');
 
+            const name = $(nameEl).text();
+            const pos = $(posEl).text();
+            const pickNum = +($(pickNumEl).text());
+            const team = $(teamEl).text();
+            const age = +($(ageEl).text());
+            const yearsPlayed = 2020 - (+($(yearsPlayedEl).text()));
+            const college = $(collegeEl).text();
+            const profileUrl = 'https://www.pro-football-reference.com' + $(nameEl).attr('href');;
 
             const dataRow = {
                 name,
@@ -41,7 +56,6 @@ async function draftLists(page) {
             }
             data.push(dataRow)
         })
-        
         await sleep(10000);
     }
     return data;
@@ -53,7 +67,7 @@ async function sleep(ms) {
 }
 
 // CUSTOM WRITEFILE
-async function writeFile(listings) {
+async function writeFile(drafts) {
     fs.writeFile('./data.json', JSON.stringify(drafts, null, 4), (err) => {
         console.log('Write-file success');
     });
